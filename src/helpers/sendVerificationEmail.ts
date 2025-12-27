c60d4e95b48680c52a85609eb5c8441ef9a1d67c
Aman Suffa <amansuffah@gmail.com>
2025-12-27 23:40:15 +0500

    fix: sign-up error

--- src/helpers/sendVerificationEmail.ts
+++ src/helpers/sendVerificationEmail.ts
@@ -13,7 +13,7 @@ export async function sendVerificationEmail(
 
 await resend.emails.send({
   from: 'onboarding@resend.dev',
-  to: 'email',
+  to: email,
   subject: 'Mystery Msg | Verification code',
   react: VerificationEmail({ username, otp: verifyCode }),
 });
